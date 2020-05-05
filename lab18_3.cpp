20f6c6d082d2c44b9c589d06c0652b5678e68521
patcharapon manuspong <59276850+aomsin1822544@users.noreply.github.com>
2020-05-05 13:14:16 +0700

    Update lab18_3.cpp

--- lab18_3.cpp
+++ lab18_3.cpp
@@ -30,11 +30,3 @@ int main(){
 	
 	return 0;
 }
-int main(){	
-	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
-	
-	cout << "There are " << count(data,sizeof(data)/sizeof(int)); 
-	cout << " different numbers in data.";
-	
-	return 0;
-}
